25ea3311906874c5ecc048972705807e0dd8e59c
Jguilhermefigueiredo <142694333+Jguilhermefigueiredo@users.noreply.github.com>
2025-07-11 00:18:35 -0300

    Add files via upload

---
+++ UsoDoVar.java
@@ -0,0 +1,12 @@
+public class UsoDoVar {
+    public static void main(String[] args) {
+        //var é usado para declarar uma variável
+        //inferência de valor
+        var numero = 10; //inferência int
+        //var usuario = new Usuario(); // inferência de tipo de usuário
+        System.out.println("Valor de número:" + numero);
+        
+    }
+}
+//variáveis locais
+//não aceita valores nulos
---
+++ UsoDoVar2.java
@@ -0,0 +1,9 @@
+public class UsoDoVar2 {
+    public static void main(String[] args) {
+        var numero = 10.5; //inferência double
+        double numero2 = numero;
+        System.out.println("Valor de número: " + numero);
+        
+    }
+
+}
