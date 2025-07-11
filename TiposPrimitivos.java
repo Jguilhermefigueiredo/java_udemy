f023e089d7193e21cdd602d58273f3f6921aaf7c
Jguilhermefigueiredo <142694333+Jguilhermefigueiredo@users.noreply.github.com>
2025-07-10 23:16:30 -0300

    Add files via upload

---
+++ TiposPrimitivos.java
@@ -0,0 +1,23 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        byte idade = 25;      
+        short ano = 2025;
+        int populacaoCidade = 500000;
+        long populacaoMundial = 7000000000L;
+        float altura = 1.75f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char initialNome = 'a';
+
+        System.out.println("Idade: "  + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População da cidade: " + populacaoCidade);
+        System.out.println("População mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salário: " + salario);
+        System.out.println("É esudante? " + estudante);
+        System.out.println("Inicial do nome: " +initialNome);
+
+    }
+    
+}
