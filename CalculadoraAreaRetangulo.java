733f76d8abd3be40224a492e95e803df4d688710
Jguilhermefigueiredo <142694333+Jguilhermefigueiredo@users.noreply.github.com>
2025-07-11 16:19:06 -0300

    Add files via upload

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,11 @@
+public class CalculadoraAreaRetangulo {
+    //segundo exercício java curso udemy
+    public static void main(String[] args) {
+        double base = 5.0;
+        double altura = 3.0;
+        double c = base * altura;
+                System.out.println("A área do retângulo é: " + c);
+        
+    }
+
+}
---
+++ ExercicioPromocao.java
@@ -0,0 +1,28 @@
+public class ExercicioPromocao {
+    //terceiro exercício curso java udemy
+    public static void main(String[] args){;
+
+    byte a = 1;
+    short b = 9;
+    char c = 'J';
+    int d = 10;
+    long e = 456789L;
+    float f = 1.75f;
+    double g = 45.45;
+    double resultado1 = g * a;
+    float resultado2 = f + b;
+    long resultado3 = e + a;
+    int resultado4 = d * b;
+    char resultado5 = c;
+    int resultado6 = a + a;
+    int resultado7 = b - b;
+        
+    System.out.println("Resultado 1 (45.45 * 1) = " + resultado1);
+    System.out.println("Resultado 2 (1.75 + 9) = " + resultado2);
+    System.out.println("Resultado 3 (456789 + 1) = " + resultado3);
+    System.out.println("Resultado 4 (10 * 9) = " + resultado4);
+    System.out.println("Resultado 5 = " + resultado5);
+    System.out.println("Resultado 6 (1 + 1)  = " + resultado6);
+    System.out.println("Resultado 7 (9 - 9) = " + resultado7);
+    }
+}
