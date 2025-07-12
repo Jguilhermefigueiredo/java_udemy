9b578e407de47807a9e6774895f4b2bc2e99042c
Jguilhermefigueiredo <142694333+Jguilhermefigueiredo@users.noreply.github.com>
2025-07-12 16:23:06 -0300

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    //loops são usados para repetir um bloco do código várias vezes.
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+
+}
