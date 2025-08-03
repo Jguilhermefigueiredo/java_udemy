d0cacbc308797fdd8a37cd199f7d3c4dcfd88216
José Guilherme Figueiredo Cavalcante <142694333+Jguilhermefigueiredo@users.noreply.github.com>
2025-08-03 14:26:49 -0300

    Add files via upload

---
+++ Pedido.java
@@ -0,0 +1,46 @@
+public class Pedido implements PedidoRestaurnte {
+    String item;
+    double preco;
+
+    public Pedido(){
+
+    }
+    public Pedido (double preco){
+        this.preco = preco;
+    }
+    public Pedido(String item, double preco){
+       this.item = item;
+       this.preco = preco;
+    }
+    public void adicionarItem(){
+        System.out.println("Item e valor do cardápio: " + item + " " + preco);
+    }
+    public double getPreco(){
+        return preco;
+    }
+  
+
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido("Hamburger", 2.50);
+        Pedido pedido1 = new Pedido("Batata", 1.50);
+        Pedido pedido2 = new Pedido("Refrigerante", 2.00);
+        pedido.adicionarItem();
+        pedido1.adicionarItem();
+        pedido2.adicionarItem();
+        pedido.getPreco();
+        pedido2.getPreco();
+
+        double calcularTotal(){
+        return pedido.getPreco() + pedido2.getPreco();
+        System.out.println("Valor final do pedido é : " + calcularTotal);
+        }
+
+
+    }
+
+}
+interface PedidoRestaurnte {
+    void adicionarItem();
+    //double calcularTotal();
+
+}
